b5dc33faf6974aab6be8febb3cc42de1122b61f0
Lubaluckshmi <lubov.loskutova@mail.ru>
2023-04-18 18:17:51 +0300

    Решили 9 задачу и добавили файл GB_java_s01.txt

--- src/main/java/lesson1/MainClassFromLessonOne.java
+++ src/main/java/lesson1/MainClassFromLessonOne.java
@@ -3,17 +3,20 @@
 // 2 Получит введенную строку и выведет в консоль сообщение “Привет, <Имя>!”
 package lesson1;
 
+import java.nio.charset.Charset;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.io.*;
 
 public class MainClassFromLessonOne {
     public static void main(String[] args) {
         //ex1();
 //        ex2();
 //        ex3();
-        ex4();
+  //      ex4();
+//        ex5(3,5);
+        ex6("Kate\tSmith\t20\nPaul\tGreen\t25\nMike\tBlack\t23\n");
     }
-
 
     //    private static void ex1(String[] args) { // cmd + alt +m -- вынести метод
 //        Scanner myScanner = new Scanner(System.in);
@@ -78,5 +81,33 @@ public class MainClassFromLessonOne {
         for (String j : words) {
             System.out.print(j + " ");
         }
+    }
+//    Реализовать функцию возведения числа а в степень b. a, b из Z.
+//    Сводя количество выполняемых действий к минимуму.
+//    Пример 1: а = 3, b = 2, ответ: 9
+//    Пример 2: а = 2, b = -2, ответ: 0.25
+//    Пример 3: а = 3, b = 0, ответ: 1
+
+    private static void ex5(int a, int b) {
+        System.out.println(Math.pow(a, b));
+    }
+/*Записать в файл следующие данные:
+
+Name Surname Age
+Kate Smith 20
+Paul Green 25
+Mike Black 23
+
+*/
+    private static void ex6(String str) {
+        try (FileWriter fw = new FileWriter("GB_java_s01.txt", Charset.forName("UTF-8"), false)) {
+            fw.write("Name\tSurname\tAge\n");
+            fw.append(str);
+            fw.flush();
+
+        } catch (Exception ex) {
+            System.out.println("Some error: " + ex.getMessage());
+        }
+
     }
 }
